fd174b9309cb073f2c652749ed47aaf7d494f9a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-04-09 20:12:05 -0500

    Added `VoltageSystem`

---
+++ Sources/EnergyKit/VoltageSystem.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/9/20.
+//
+
+public enum VoltageSystem: Int {
+    
+    case dc12       = 12
+    case dc24       = 24
+    case dc48       = 48
+    case ac110      = 110
+    case ac220      = 220
+}
