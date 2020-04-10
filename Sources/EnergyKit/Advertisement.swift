5f3a180db4cce18b2a4794d3309afd6e217dcc11
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-04-09 20:12:50 -0500

    Added Bluetooth advertising extensions

---
+++ Sources/EnergyKit/Advertisement.swift
@@ -0,0 +1,64 @@
+//
+//  Advertisement.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/9/20.
+//
+
+import Foundation
+import Bluetooth
+
+
+#if os(macOS) || os(Linux)
+
+public extension BluetoothHostControllerInterface {
+    
+    /// LE Advertise with iBeacon
+    func setEnergyAdvertisingData(identifier: UUID, rssi: Int8, commandTimeout: HCICommandTimeout = .default) throws {
+        
+        do { try enableLowEnergyAdvertising(false) }
+        catch HCIError.commandDisallowed { }
+        
+        let beacon = AppleBeacon(uuid: lock, rssi: rssi)
+        let flags: GAPFlags = [.lowEnergyGeneralDiscoverableMode, .notSupportedBREDR]
+        
+        try iBeacon(beacon, flags: flags, interval: .min, timeout: commandTimeout)
+        
+        do { try enableLowEnergyAdvertising() }
+        catch HCIError.commandDisallowed { }
+    }
+    
+    /// LE Scan Response
+    func setEnergyScanResponse(commandTimeout: HCICommandTimeout = .default) throws {
+        
+        do { try enableLowEnergyAdvertising(false) }
+        catch HCIError.commandDisallowed { }
+        
+        let name: GAPCompleteLocalName = "Lock"
+        let serviceUUID: GAPIncompleteListOf128BitServiceClassUUIDs = [UUID(bluetooth: LockService.uuid)]
+        
+        let encoder = GAPDataEncoder()
+        let data = try encoder.encodeAdvertisingData(name, serviceUUID)
+        
+        try setLowEnergyScanResponse(data, timeout: commandTimeout)
+        
+        do { try enableLowEnergyAdvertising() }
+        catch HCIError.commandDisallowed { }
+    }
+    
+    /// LE Advertise with iBeacon for data changed
+    func setEnergyNotificationAdvertisement(rssi: Int8, commandTimeout: HCICommandTimeout = .default) throws {
+        
+        do { try enableLowEnergyAdvertising(false) }
+        catch HCIError.commandDisallowed { }
+        
+        let beacon = AppleBeacon(uuid: .lockNotificationBeacon, rssi: rssi)
+        let flags: GAPFlags = [.lowEnergyGeneralDiscoverableMode, .notSupportedBREDR]
+        try iBeacon(beacon, flags: flags, interval: .min, timeout: commandTimeout)
+        
+        do { try enableLowEnergyAdvertising() }
+        catch HCIError.commandDisallowed { }
+    }
+}
+
+#endif
